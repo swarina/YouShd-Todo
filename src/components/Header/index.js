fc74a580e531d6d8d4b4c2358e36ebfbc88d8395
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-26 04:12:21 +0530

    Add: Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,28 @@
 // CSS
 import "./index.css";
 
+// Image
+import Image from "../../assets/images/profile.png";
+
+// Icons
+import { BiSearch } from "react-icons/bi";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header-container">
+      <div className="header-search">
+        <span>
+          <BiSearch />
+        </span>
+        <input type="text" name="search" placeholder="Search" />
+      </div>
+      <div className="header-participants">Hello</div>
+      <div className="header-info">
+        <p>Hi Saundarya</p>
+        <img src={Image} alt="profile" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;

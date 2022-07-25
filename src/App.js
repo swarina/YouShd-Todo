caba11d5078e7f17b95f5ed85df63129dd2d746c
Swarina Jaiswal <swarinajaiswal@gmail.com>
2022-07-25 21:43:02 +0530

    Setup: routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
+// Components
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+// Route
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
 }
 
 export default App;
